283c92804eadefc89f8c9ddbe51d83a95226bfa3
noah <nore5515@gmail.com>
2022-11-18 10:41:25 -0600

    Added two line (plotted) graph

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -34,6 +34,7 @@ namespace WpfApp1
     {
         public MainViewModel()
         {
+            this.TwoColorModel = TwoColorLineSeries();
             this.MyModel = new PlotModel { Title = "Example 1" };
             this.MyModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
 
@@ -54,8 +55,25 @@ namespace WpfApp1
             this.MyModel.Annotations.Add(Line);
         }
 
+        public static PlotModel TwoColorLineSeries()
+        {
+            var model = new PlotModel { Title = "TwoColorLineSeries" };
+            var twoColorLineSeries = new TwoColorLineSeries
+            {
+                Color = OxyColors.Red,
+                Color2 = OxyColors.Blue,
+                LineStyle = LineStyle.Solid,
+                LineStyle2 = LineStyle.Dot
+            };
+            twoColorLineSeries.Points.Add(new DataPoint(0, -6));
+            twoColorLineSeries.Points.Add(new DataPoint(10, 4));
+            twoColorLineSeries.Points.Add(new DataPoint(30, -2));
+            twoColorLineSeries.Points.Add(new DataPoint(40, 8));
+            model.Series.Add(twoColorLineSeries);
+            return model;
+        }
 
-
+        public PlotModel TwoColorModel { get; private set; }
         public PlotModel MyModel { get; private set; }
     }
 
